464f823e0d81c0c52135105c73221bc9500c1d99
saffron <d.s.tadiyal95@gmail.com>
2018-04-30 19:26:30 +0530

    Implementation of BottomNavigationView has condition: when there is more than 3 items then use shift mode.
    At this moment you cannot change it through existing API and the only way to disable shift mode is to use reflection.

---
+++ app/src/main/java/info/androidhive/bottomnavigation/helper/BottomNavigationBehavior.java
@@ -0,0 +1,79 @@
+package info.androidhive.bottomnavigation.helper;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by ravi on 21/12/17.
+ */
+
+public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomNavigationView> {
+
+    public BottomNavigationBehavior() {
+        super();
+    }
+
+    public BottomNavigationBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @SuppressLint("RestrictedApi")
+    public static void removeShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field");
+        } catch (IllegalAccessException e) {
+            Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode");
+        }
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, BottomNavigationView child, View dependency) {
+        boolean dependsOn = dependency instanceof FrameLayout;
+        return dependsOn;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, BottomNavigationView child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, BottomNavigationView child, View target, int dx, int dy, int[] consumed) {
+        if (dy < 0) {
+            showBottomNavigationView(child);
+        } else if (dy > 0) {
+            hideBottomNavigationView(child);
+        }
+    }
+
+    private void hideBottomNavigationView(BottomNavigationView view) {
+        view.animate().translationY(view.getHeight());
+    }
+
+    private void showBottomNavigationView(BottomNavigationView view) {
+        view.animate().translationY(0);
+    }
+}
